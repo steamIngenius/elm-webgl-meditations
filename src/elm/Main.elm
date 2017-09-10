f7ae13aed04a9596bbc82fb1177714fca022b71d
Micah Hawkins <micahbhawkins@gmail.com>
2017-09-09 22:11:33 -0700

    Window fills entire screen, resizes to fit

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,17 +1,70 @@
 module Main exposing (main)
 
+import Window exposing (Size)
+import Html exposing (Html)
 import Html.Attributes as Att
 import WebGL exposing (toHtml)
+import Task
 
 
 main =
+    Html.program
+        { init = init
+        , subscriptions = \_ -> Window.resizes WindowResize
+        , update = update
+        , view = view
+        }
+
+
+
+-- MODEL --
+
+
+type alias Model =
+    { size : Size
+    }
+
+
+
+-- INIT --
+
+
+init =
+    ( Model (Size 0 0), Task.perform WindowResize Window.size )
+
+
+
+-- UPDATE --
+
+
+type Msg
+    = WindowResize Size
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        WindowResize size ->
+            let
+                _ =
+                    Debug.log "Current window size: " size
+            in
+                ( Model size, Cmd.none )
+
+
+
+-- VIEW --
+
+
+view model =
     toHtml
         [ Att.id "canvas"
-        , Att.width 400
-        , Att.height 200
+        , Att.width model.size.width
+        , Att.height model.size.height
         , Att.style
-            [ ( "width", toString 400 ++ "px" )
-            , ( "height", toString 200 ++ "px" )
+            [ ( "display", "block" )
+            , ( "width", toString model.size.width ++ "px" )
+            , ( "height", toString model.size.height ++ "px" )
             , ( "background", "#000" )
             ]
         ]
